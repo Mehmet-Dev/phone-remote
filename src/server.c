25e5b66306e5faac3e3eae5ccf637773aeaaca3d
Mehmet-Dev <m.e.tseven@outlook.com>
2026-03-16 01:32:00 +0100

    socket creation functionality added

---
+++ src/server.c
@@ -0,0 +1,25 @@
+#include "server.h"
+
+int create_socket() {
+    int creation = socket(AF_INET, SOCK_DGRAM, 0); // create a socket
+
+    if(creation == -1) { // in the case it fails for some reason
+        perror("Some error occured and the port couldn't be created sorry");
+        return 0;
+    }
+
+    struct sockaddr_in server_addr; // setting up the actual address for the socket
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(DEFAULT_PORT);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    int created = bind(creation, (struct sockaddr*)&server_addr, sizeof(server_addr));
+
+    if(created < 0) { // if it doesn't bind then yea fuck off
+        perror("Couldn't bind socket as the address might be already in use or permission denied");
+        return 0;
+    }
+
+    return creation; // return the id of the descriptor on success
+}
